9e1677fd0eaf9bd06cce87753b9d9464462539dd
Evren Akgün <evrenakgun3@gmail.com>
2022-07-12 20:19:34 +0300

    koşullarda güncellemeler yapıldı.

--- src/kucuktenBuyugeSiralama/KucuktenBuyugeSiralama.java
+++ src/kucuktenBuyugeSiralama/KucuktenBuyugeSiralama.java
@@ -18,32 +18,56 @@ public class KucuktenBuyugeSiralama {
 		System.out.print("3. sayi: ");
 		sayi3 = input.nextInt();
 		
-		if (sayi1 <= sayi2 && sayi1 <= sayi3) {
-			if (sayi2 <= sayi3) {
+		if (sayi1 < sayi2 && sayi1 < sayi3) {
+			if (sayi2 < sayi3) {
 				System.out.println(sayi1 + " < " + sayi2 + " < " + sayi3);
+			}
+			else if (sayi2 == sayi3) {
+				System.out.println(sayi1 + " < " + sayi2 + " = " + sayi3);
 			}
 			else {
 				System.out.println(sayi1 + " < " + sayi3 + " < " + sayi2);
 			}
 		}
 		
-		if (sayi2 <= sayi1 && sayi2 <= sayi3) {
-			if (sayi1 <= sayi3) {
+		else if (sayi2 < sayi1 && sayi2 < sayi3) {
+			if (sayi1 < sayi3) {
 				System.out.println(sayi2 + " < " + sayi1 + " < " + sayi3);
+			}
+			else if (sayi1 == sayi3) {
+				System.out.println(sayi2 + " < " + sayi1 + " = " + sayi3);
 			}
 			else {
 				System.out.println(sayi2 + " < " + sayi3 + " < " + sayi1);
 			}
 		}
 		
-		if (sayi3 <= sayi2 && sayi3 <= sayi1) {
-			if (sayi2 <= sayi1) {
+		else if (sayi3 < sayi2 && sayi3 < sayi1) {
+			if (sayi2 < sayi1) {
 				System.out.println(sayi3 + " < " + sayi2 + " < " + sayi1);
+			}
+			else if (sayi2 == sayi1) {
+				System.out.println(sayi3 + " < " + sayi2 + " = " + sayi1);
 			}
 			else {
 				System.out.println(sayi3 + " < " + sayi1 + " < " + sayi2);
 			}
 		}
 		
+		else if (sayi1 == sayi2 && sayi1 < sayi3) {
+			System.out.println(sayi1 + " = " + sayi2 + " < " + sayi3);
+		}
+		
+		else if (sayi2 == sayi3 && sayi3 < sayi1) {
+			System.out.println(sayi3 + " = " + sayi2 + " < " + sayi1);
+		}
+		
+		else if (sayi1 == sayi3 && sayi1 < sayi2) {
+			System.out.println(sayi1 + " = " + sayi3 + " < " + sayi2);
+		}
+		
+		else if (sayi1 == sayi2 && sayi2 == sayi3) {
+			System.out.println(sayi1 + " = " + sayi2 + " = " + sayi3);
+		}
 	}
 }
